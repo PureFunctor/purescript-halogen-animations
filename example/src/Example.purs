1ab92a54e1b69dce82d32b41d0cd97b29503655f
PureFunctor <purefunctor@gmail.com>
2020-12-31 00:09:35 +0800

    Use integers as slot indices

--- example/src/Example.purs
+++ example/src/Example.purs
@@ -15,13 +15,13 @@ css :: forall a r. String -> HP.IProp (class :: String | r) a
 css = HP.class_ <<< H.ClassName
 
 
-type Slots = (animated_box :: HN.Slot Void Unit)
+type Slots = (animated_box :: HN.Slot Void Int)
 
 
 _animated_box = SProxy :: SProxy "animated_box"
 
 
-data Action = Clicked
+data Action = Clicked Int
 
 
 component :: forall query input output m. H.Component HH.HTML query input output m
@@ -37,8 +37,8 @@ component =
 
 render :: forall state m. state -> HH.ComponentHTML Action Slots m
 render _ =
-  HH.div [ css "example" , HE.onClick \_ -> Just Clicked ]
-  [ HH.slot _animated_box unit HN.component
+  HH.div [ css "example" , HE.onClick \_ -> Just $ Clicked 0 ]
+  [ HH.slot _animated_box 0 HN.component
     { start: "opened"
     , toFinal: "move-and-lock"
     , final: "locked"
@@ -50,6 +50,4 @@ render _ =
 
 handleAction :: forall state output m. Action -> H.HalogenM state Action Slots output m Unit
 handleAction = case _ of
-  Clicked -> do
-    _ <- H.query _animated_box unit $ H.tell HN.ToggleAnimation
-    pure unit
+  (Clicked i) -> void $ H.query _animated_box i $ H.tell HN.ToggleAnimation
