bacac276ca1e430df326e960379770c3d4777881
PureFunctor <purefunctor@gmail.com>
2020-12-31 00:08:07 +0800

    Extract render and handleAction to the top level

--- example/src/Example.purs
+++ example/src/Example.purs
@@ -15,6 +15,9 @@ css :: forall a r. String -> HP.IProp (class :: String | r) a
 css = HP.class_ <<< H.ClassName
 
 
+type Slots = (animated_box :: HN.Slot Void Unit)
+
+
 _animated_box = SProxy :: SProxy "animated_box"
 
 
@@ -30,19 +33,23 @@ component =
     { handleAction = handleAction
     }
   }
-  where
-  render _ =
-    HH.div [ css "example" , HE.onClick \_ -> Just Clicked ]
-    [ HH.slot _animated_box unit HN.component
-      { start: "opened"
-      , toFinal: "move-and-lock"
-      , final: "locked"
-      , toStart: "move-and-open"
-      , render: HH.div [ css "box" ] [ ]
-      } absurd
-    ]
 
-  handleAction = case _ of
-    Clicked -> do
-      _ <- H.query _animated_box unit $ H.tell HN.ToggleAnimation
-      pure unit
+
+render :: forall state m. state -> HH.ComponentHTML Action Slots m
+render _ =
+  HH.div [ css "example" , HE.onClick \_ -> Just Clicked ]
+  [ HH.slot _animated_box unit HN.component
+    { start: "opened"
+    , toFinal: "move-and-lock"
+    , final: "locked"
+    , toStart: "move-and-open"
+    , render: HH.div [ css "box" ] [ ]
+    } absurd
+  ]
+
+
+handleAction :: forall state output m. Action -> H.HalogenM state Action Slots output m Unit
+handleAction = case _ of
+  Clicked -> do
+    _ <- H.query _animated_box unit $ H.tell HN.ToggleAnimation
+    pure unit
